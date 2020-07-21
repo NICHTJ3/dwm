53620e020afcf12873bf38df800509e2c1beeb7a
NICHTJ3 <nicholsontrent@gmail.com>
2020-07-21 16:17:30 +1200

    Change layout symbols

--- config.h
+++ config.h
@@ -3,13 +3,17 @@
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar = 1;           /* 0 means no bar */
-static const int gappx = 6;             /* 0 means no gaps */
-static const int topbar = 1;            /* 0 means bottom bar */
+static const unsigned int systraypinning =
+    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+          X */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst =
+    1; /* 1: if pinning fails, display systray on the first monitor, False:
+          display systray on the last monitor*/
+static const int showsystray = 1; /* 0 means no systray */
+static const int showbar = 1;     /* 0 means no bar */
+static const int gappx = 6;       /* 0 means no gaps */
+static const int topbar = 1;      /* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
 static const char norm_bg[] = "#2b2e3b";
@@ -45,8 +49,8 @@ static const int resizehints =
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"[]=", tile}, /* first entry is default */
-    {"><>", NULL}, /* no layout function means floating behavior */
+    {"[T]=", tile}, /* first entry is default */
+    {"[F]", NULL},  /* no layout function means floating behavior */
     {"[M]", monocle},
 };
 
